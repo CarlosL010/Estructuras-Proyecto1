459a0cafa5c009173169bb9fa74bca2d3013ee5e
CarlosL010 <114618044+CarlosL010@users.noreply.github.com>
2024-06-09 21:29:00 -0400

    Add files via upload

---
+++ src/Proyecto1_Manuel_Novoa_Carlos_Gonzalez/Proyecto1EDD.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto1_Manuel_Novoa_Carlos_Gonzalez;
+import Interfaces.INICIO;
+
+public class Proyecto1EDD {
+
+    public static void main(String[] args) {
+        INICIO b = new INICIO();
+        b.setVisible(true);
+    }
+    
+}
